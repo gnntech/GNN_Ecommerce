e5887a03fefa2c1a7ff75aecad27b4ba92c84655
Renuka Berad <renukab290906@gmail.com>
2026-02-19 12:58:49 +0530

    Resolved .env conflict, keeping local MONGO_URI

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+  baseURL: "https://gnn-ecommerce-1.onrender.com/api",
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const storedUser = localStorage.getItem("adminUser");
-        if (storedUser) {
-            const { token } = JSON.parse(storedUser);
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const storedUser = localStorage.getItem("adminUser");
+    if (storedUser) {
+      const { token } = JSON.parse(storedUser);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
 );
 
 export default api;
