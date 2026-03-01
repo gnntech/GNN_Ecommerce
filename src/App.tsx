f61c83ceedd88097aa67c5b3db13314df13e84e5
Renuka Berad <renukab290906@gmail.com>
2026-03-01 23:02:02 +0530

    The about page is updated with correct routing

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ const App = () => (
               />
               <Route path="/admin/gallery" element={<ManageGallery />} />
               <Route path="/admin/marquee" element={<ManageMarquee />} />
-
+              <Route path="/about" element={<About />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
